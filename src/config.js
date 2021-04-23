62d1880b0fc1db75ad4233396425c1cf25c2e102
Vendy Prum <vendy@thinkful.com>
2021-04-22 19:50:32 -0700

    update config file for ssl

--- src/config.js
+++ src/config.js
@@ -4,6 +4,6 @@ module.exports = {
 
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/acnh_museum_donation_tracker',
+  DATABASE_URL: process.env.DATABASE_URL + '?ssl=true' || 'postgresql://postgres@localhost/acnh_museum_donation_tracker',
   JWT_SECRET:process.env.JWT_SECRET || 'acnh_secret'
 }
